f30babab93013807687dd5503fa123dd9cdaf5ef
InnerPeace080 <tranphuong.080@gmail.com>
2016-11-09 14:13:03 +0700

    change inden

--- src/actions/RDActions_MiddleWare.js
+++ src/actions/RDActions_MiddleWare.js
@@ -32,96 +32,96 @@ class RDActions_MiddleWare {
       var obj = self.actionsList[key];
       // CREATE MIDDLEWARE FUNCTION
       self[key] = function(arg={},setState = true) {
-                    var actionName = key;
-                    var query = obj.query;
-                    var argTemp = Util.dataProtectAndMap(arg, obj.argFormat);
+        var actionName = key;
+        var query = obj.query;
+        var argTemp = Util.dataProtectAndMap(arg, obj.argFormat);
 
-                    var preTextLog = self.name+':'+actionName+':';
-                    return (dispatch, getState) => {
-                      //
-                      var retFromPreProcess;
-                      if (obj.preProcess) {
-                        retFromPreProcess = obj.preProcess(argTemp,getState);
+        var preTextLog = self.name+':'+actionName+':';
+        return (dispatch, getState) => {
+          //
+          var retFromPreProcess;
+          if (obj.preProcess) {
+            retFromPreProcess = obj.preProcess(argTemp,getState);
+          }
+          if (retFromPreProcess) {
+            return retFromPreProcess;
+          }
+          var req = {};
+          // req config
+          if (obj.onArg) {
+            req = obj.onArg(argTemp,getState);
+          }else{
+            req=argTemp;
+          }
+
+          //
+          Debug.log(preTextLog+':'+query+':'+JSON.stringify(req));
+          var promise = new Promise((resolve,reject)=>{
+            var data ={};
+            // check limitProcess
+            if ( getState()[this.sortName] && getState()[this.sortName][actionName]  &&
+              getState()[this.sortName][actionName].loading >= obj.limitProcess) {
+              Debug.log(preTextLog+':reach limitProcess:' + getState()[this.sortName][actionName].loading);
+              data={
+                arg:argTemp,
+                err:'react limitProcess',
+              }
+              reject(data)
+              return;
+            }
+
+            // check connection
+            if (socketConnection.getConnectState()) {
+              if(setState) {dispatch(RDActions[this.sortName][actionName+'OnRequest']()); }
+              socketConnection.emit(query,req,
+                  TimeoutCallback(Define.constants.requestTimeout,(err,res) => {
+                    Debug.log(preTextLog+':callback:'+query+':'+JSON.stringify(req));
+                    if (err){
+                      Debug.log(preTextLog +'err:',Debug.level.ERROR);
+                      // Debug.log(err);
+                      if (err == 'Error: callback timeout') {
+                        Debug.log(preTextLog+'callback timeout',Debug.level.ERROR);
                       }
-                      if (retFromPreProcess) {
-                        return retFromPreProcess;
+                      data={
+                        arg:argTemp,
+                        err:err,
                       }
-                      var req = {};
-                      // req config
-                      if (obj.onArg) {
-                        req = obj.onArg(argTemp,getState);
+                      var onErrorRet = true;
+                      if (obj.onError) {onErrorRet = obj.onError(dispatch,getState,data);}
+                      if (onErrorRet) {
+                        if(setState) {dispatch(RDActions[this.sortName][actionName+'OnResult'](RDActionsTypes.constants.REQUEST_SUBTYPE.ERROR,data));}
+                        reject(data);
                       }else{
-                        req=argTemp;
+                        if(setState) {dispatch(RDActions[this.sortName][actionName+'OnResult'](RDActionsTypes.constants.REQUEST_SUBTYPE.SUCCESS,data));}
+                        resolve(data);
                       }
-
-                      //
-                      Debug.log(preTextLog+':'+query+':'+JSON.stringify(req));
-                      var promise = new Promise((resolve,reject)=>{
-                        var data ={};
-                        // check limitProcess
-                        if ( getState()[this.sortName] && getState()[this.sortName][actionName]  &&
-                          getState()[this.sortName][actionName].loading >= obj.limitProcess) {
-                          Debug.log(preTextLog+':reach limitProcess:' + getState()[this.sortName][actionName].loading);
-                          data={
-                            arg:argTemp,
-                            err:'react limitProcess',
-                          }
-                          reject(data)
-                          return;
-                        }
-
-                        // check connection
-                        if (socketConnection.getConnectState()) {
-                          if(setState) {dispatch(RDActions[this.sortName][actionName+'OnRequest']()); }
-                          socketConnection.emit(query,req,
-                              TimeoutCallback(Define.constants.requestTimeout,(err,res) => {
-                                Debug.log(preTextLog+':callback:'+query+':'+JSON.stringify(req));
-                                if (err){
-                                  Debug.log(preTextLog +'err:',Debug.level.ERROR);
-                                  // Debug.log(err);
-                                  if (err == 'Error: callback timeout') {
-                                    Debug.log(preTextLog+'callback timeout',Debug.level.ERROR);
-                                  }
-                                  data={
-                                    arg:argTemp,
-                                    err:err,
-                                  }
-                                  var onErrorRet = true;
-                                  if (obj.onError) {onErrorRet = obj.onError(dispatch,getState,data);}
-                                  if (onErrorRet) {
-                                    if(setState) {dispatch(RDActions[this.sortName][actionName+'OnResult'](RDActionsTypes.constants.REQUEST_SUBTYPE.ERROR,data));}
-                                    reject(data);
-                                  }else{
-                                    if(setState) {dispatch(RDActions[this.sortName][actionName+'OnResult'](RDActionsTypes.constants.REQUEST_SUBTYPE.SUCCESS,data));}
-                                    resolve(data);
-                                  }
-                                }
-                                else{
-                                  Debug.log(preTextLog +'res:');
-                                  if (self.logRes) {Debug.log(res);}
-                                  data={
-                                    arg:argTemp,
-                                    res:res,
-                                  }
-                                  var onDoneRet = true;
-                                  if (obj.onDone) {onDoneRet = obj.onDone(dispatch,getState,data);}
-                                  if (onDoneRet) {
-                                    if(setState) {dispatch(RDActions[this.sortName][actionName+'OnResult'](RDActionsTypes.constants.REQUEST_SUBTYPE.SUCCESS,data));}
-                                    resolve(data);
-                                  }else{
-                                    if(setState) {dispatch(RDActions[this.sortName][actionName+'OnResult'](RDActionsTypes.constants.REQUEST_SUBTYPE.ERROR,data));}
-                                    reject(data);
-                                  }
-                                }
-                              }
-                            )
-                          );
-                        }
-                        else{reject()}
-                      })
-                      return promise;
                     }
-                  };
+                    else{
+                      Debug.log(preTextLog +'res:');
+                      if (self.logRes) {Debug.log(res);}
+                      data={
+                        arg:argTemp,
+                        res:res,
+                      }
+                      var onDoneRet = true;
+                      if (obj.onDone) {onDoneRet = obj.onDone(dispatch,getState,data);}
+                      if (onDoneRet) {
+                        if(setState) {dispatch(RDActions[this.sortName][actionName+'OnResult'](RDActionsTypes.constants.REQUEST_SUBTYPE.SUCCESS,data));}
+                        resolve(data);
+                      }else{
+                        if(setState) {dispatch(RDActions[this.sortName][actionName+'OnResult'](RDActionsTypes.constants.REQUEST_SUBTYPE.ERROR,data));}
+                        reject(data);
+                      }
+                    }
+                  }
+                )
+              );
+            }
+            else{reject()}
+          })
+          return promise;
+        }
+      };
     })
     return self;
   }
