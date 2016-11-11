a10c107c4990ad002123df4469a52a84b0b55451
Hoang Tuan <tuanhm@gviet.vn>
2016-11-11 14:36:15 +0700

    import instead require

--- src/Include.js
+++ src/Include.js
@@ -2,11 +2,12 @@
 /**
 *   NOTE: file have been require in here can not require this file
 */
+import ScrollView from './components/elements/CustomScrollView'
 
 const Include = {
     Image:require('./components/elements/CustomImage'),
     Text:require('./components/elements/CustomText'),
-    ScrollView:require('./components/elements/CustomScrollView'),
+    ScrollView
 }
 
 module.exports = Include;
